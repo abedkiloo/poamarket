836a3db366217abac54b2f83a3efdc306f99bfa3
Abednego Wambua <abedxh@gmail.com>
2020-06-08 18:23:49 +0300

    contact us page update

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -75,6 +75,57 @@
 </div> <!-- ./category-area -->
 
 
+
+<!-- Contact page main form area start -->
+
+<div class="contact-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mb-5 mb-lg-0">
+                <div class="sc-title">
+                    <h3>Drop us a message</h3>
+                </div>
+                <div class="contact-main-form">
+                    <form action="">
+                        <div class="single-inp-box">
+                            <input type="text" placeholder="Name *">
+                        </div>
+                        <div class="single-inp-box">
+                            <input type="text" placeholder="Email *">
+                        </div>
+                        <div class="single-inp-box">
+                            <input type="text" placeholder="Subject *">
+                        </div>
+                        <div class="single-inp-box">
+                            <textarea name="" id="" cols="30" rows="10" placeholder="Comment"></textarea>
+                        </div>
+                        <div class="sb-btn">
+                            <button type="submit">SEND</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-5 offset-lg-1">
+                <div class="sc-title">
+                    <h3>Help Center</h3>
+                </div>
+                <div class="help-text-list">
+                    <ul>
+                        <li><span>1.</span>Log into your Betapia account.</li>
+                        <li><span>2.</span>Hover the mouse over your username at the top of the screen.</li>
+                        <li><span>3.</span>Click ‘Downloads’ from the drop down menu.`</li>
+                        <li><span>4.</span>Click ‘License certificate & purchase code’ (available as PDF or text file).</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Contact page main form area end -->
+
+
 <!-- Contact Us Area End-->
 
 @endsection
